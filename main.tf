49f43fedd675f95d0a34b54dc369aab470ea77ee
ravindrarockz9999 <ravindrarockz9999@gmail.com>
2026-04-12 04:21:42 +0000

    added-vm

--- main.tf
+++ main.tf
@@ -8,3 +8,16 @@ resource "google_storage_bucket" "bucket"{
     location = var.region
     uniform_bucket_level_access = true
 }
+
+resource "google_compute_instance" "vm"{
+    name = "github-vm"
+    zone = "us-central1-a"
+    boot_disk{
+            initialize_params {
+               image = "debian-cloud/debian11"
+            }
+    }
+    network_interface{
+        network = "default"
+    }
+}
